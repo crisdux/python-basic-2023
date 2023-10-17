2b265fb1b72d49382f8b4587c80227988e7f287a
crisdux <crisfer.4217@gmail.com>
2023-10-17 15:51:27 -0400

    string methods

---
+++ 01-basics/03-string-methods.py
@@ -0,0 +1,10 @@
+mensaje = "Hola como estas";
+nombre = "cris"
+
+print(mensaje.upper());
+print(mensaje.lower());
+
+print(nombre.capitalize())
+print(nombre.find("c"))
+print(nombre.isupper());
+print(nombre.isdigit())
