8bf657096471f0b1f33fb6b79278fece74613c58
crisdux <crisfer.4217@gmail.com>
2023-10-17 14:54:18 -0400

    primer commit

---
+++ 01-basics/01-variables.py
@@ -0,0 +1,10 @@
+entero = 22; # int
+flotante = 2.3; #float
+cadena = "Cris"; #string
+bol = True; #bool
+
+#  print all
+
+print(entero, flotante, cadena, bol);
+
+print(type(entero), type(flotante), type(cadena), type(bol));
