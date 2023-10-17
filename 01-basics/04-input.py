df4253869269dbab2ed2ef0af44849f8c5c37143
crisdux <crisfer.4217@gmail.com>
2023-10-17 16:39:08 -0400

    op logico

---
+++ 01-basics/04-input.py
@@ -0,0 +1,5 @@
+num1 = input("ingrese un numero");
+num2 = input("ingrese otro numero");
+
+res = int(num1) + int(num2); # input siempre regresa un string
+print(res)
---
+++ 01-basics/05-operadores.py
@@ -0,0 +1,7 @@
+print(5+5);
+print(5-5);
+print(5*5);
+print(10/2);
+print(20%3);
+print(5/33)
+print(5//33)
