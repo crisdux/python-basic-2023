60289b5831d7d12b9fb9e94da9572ece6b0cac73
crisdux <crisfer.4217@gmail.com>
2023-10-17 15:15:06 -0400

    cortar cadenas

--- 01-basics/02-indexacion.py
+++ 01-basics/02-indexacion.py
@@ -9,3 +9,19 @@ pais = "Bolivia";
 print(pais[0:3]) # Bol
 
 print(len(pais)) # 7
+
+mensaje = "Hola mundo, como estas el dia de hoy?";
+# Hola, como estas hoy?
+
+str1 = mensaje[:4]; # desde el inicio hasta la posicion 4
+str2 = mensaje[10:22]; # desde la posicion 10 hasta la posicion 22
+str3 = mensaje[32:] # desde la posicion 32 hasta el final
+
+print((str1 + str2 + str3)) # concatenamos para tener el resultado
+
+mensaje = "Hola mundo, como estas el dia de hoy?";
+# Hola, como estas hoy?
+
+comunicado = "Hoy es martes, y juega Bolivia";
+
+print(comunicado[::2]) # desde el inicio hasta el final, saltando de 2 en 2
