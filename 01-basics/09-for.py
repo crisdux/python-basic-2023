21c060fb1be50881e842f651436c90223e8f3dd8
crisdux <crisfer.4217@gmail.com>
2023-10-18 08:59:02 -0400

    for

---
+++ 01-basics/09-for.py
@@ -0,0 +1,17 @@
+## for para cadenas
+for i in "hola mundo":
+    print(i,end=".")
+
+## for para arreglos
+for i in [1,2,5,9,3]:
+    print(f"el numero es:{i}")
+
+## for para diccionarios
+persona = {
+    "nombre": "Cris",
+    "edad":27,
+    "sexo": "M",
+}
+for i in persona:
+    print(f"la llave es: {i} para {persona[i]}")
+
