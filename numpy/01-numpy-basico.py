d2f5a91d2e56effcea17c587c31ff66fd11ced01
crisdux <crisfer.4217@gmail.com>
2023-10-20 16:05:54 -0400

    basic numpy

--- numpy/01-numpy-basico.py
+++ numpy/01-numpy-basico.py
@@ -3,3 +3,22 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 5])
 print(arr)
 
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr2) ## matriz
+
+arr3 = np.array([1.2, 6.9, 54.3, 99.3])
+
+print(arr3) ## array de numeros flotantes
+
+arr4 = np.array(range(0,20));
+
+print(arr4) ## array de numeros enteros
+
+arr5 = np.arange(0,20);
+
+print(arr5) ## array de numeros enteros
+
+arr_zeros = np.zeros((3), int)
+
+print(arr_zeros) ## array de ceros
